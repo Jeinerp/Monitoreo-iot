b5721359bc41b6d623912dc88287aa1cf996de0b
Jeinerp <143730457+Jeinerp@users.noreply.github.com>
2026-04-29 20:30:33 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -42,13 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'rest_framework_simplejwt',
-
-    
+    'rest_framework_simplejwt',    
     'core',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -133,3 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
